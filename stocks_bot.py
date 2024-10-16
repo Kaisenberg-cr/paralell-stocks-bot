eb81fe881e914861406df05d17e69ac7a97dd205
Kairo Chacón Maleaños <kairochm@gmail.com>
2024-10-16 00:21:56 -0600

    Working version but there might be some bugs due to network delays

--- stocks_bot.py
+++ stocks_bot.py
@@ -64,22 +64,30 @@ class StocksBot:
             bool: True if login is successful, False otherwise.
         """
         try:
-            self.pyppeteer_extensions.open_web_page(
-                "https://app.koyfin.com/login", '//input[@type="email"]'
-            )
-            self.pyppeteer_extensions.set_text(
-                '//input[@type="email"]', self.login_email
-            )
-            time.sleep(2)
-            self.pyppeteer_extensions.set_text(
-                '//input[@name="password"]', self.login_password
-            )
-            time.sleep(2)
-            self.pyppeteer_extensions.click('//label[text()="Sign in"]')
-            self.pyppeteer_extensions.wait_for_element(
-                '//div[text()="Search for a name, ticker, or function "]'
-            )
-            return True
+            count = 0
+            continue_flag = False
+            while not continue_flag and count < 3:
+                self.pyppeteer_extensions.open_web_page(
+                    "https://app.koyfin.com/login", '//input[@type="email"]'
+                )
+                self.pyppeteer_extensions.set_text(
+                    '//input[@type="email"]', self.login_email
+                )
+                time.sleep(2)
+                self.pyppeteer_extensions.set_text(
+                    '//input[@name="password"]', self.login_password
+                )
+                time.sleep(2)
+                self.pyppeteer_extensions.click('//label[text()="Sign in"]')
+                self.pyppeteer_extensions.wait_for_element(
+                    '//div[text()="Search for a name, ticker, or function "]'
+                )
+                if self.pyppeteer_extensions.element_exist(
+                    '//button[text()="Accept All"]'
+                ):
+                    self.pyppeteer_extensions.click('//button[text()="Accept All"]')
+                continue_flag = True
+                return continue_flag
         except Exception as e:
             print(f"Login failed: {e}")
             return False
@@ -152,6 +160,15 @@ class StocksBot:
             ticker (str): The stock ticker symbol.
             char (int): The column character code in the Excel sheet.
         """
+        pe_ratio = ""
+        pb_ratio = ""
+        stock_price = ""
+        current_ratio = ""
+        return_on_equity = ""
+        asset_turnover = ""
+        eps_current_fiscal_year = ""
+        eps_last_fiscal_year = ""
+        total_liabilities_total_assets = ""
         self.pyppeteer_extensions.click(
             '//div[text()="Search for a name, ticker, or function "]'
         )
@@ -164,6 +181,7 @@ class StocksBot:
         self.pyppeteer_extensions.wait_for_element(
             f'(//div[@class="rc-dialog-content"]/descendant::div[text()="{ticker}"])[1]'
         )
+        time.sleep(1)
         self.pyppeteer_extensions.click(
             f'(//div[@class="rc-dialog-content"]/descendant::div[text()="{ticker}"])[1]'
         )
@@ -176,62 +194,127 @@ class StocksBot:
         self.pyppeteer_extensions.wait_for_element(
             '//div[text()="P/E"]/following-sibling::div/div/div'
         )
-        pe_ratio = self.pyppeteer_extensions.get_text(
-            '//div[text()="P/E"]/following-sibling::div/div/div'
-        )
-        time.sleep(1)
-        pb_ratio = self.pyppeteer_extensions.get_text(
+        while not pe_ratio:
+            pe_ratio = self.pyppeteer_extensions.get_text(
+                '//div[text()="P/E"]/following-sibling::div/div/div'
+            )
+        self.pyppeteer_extensions.wait_for_element(
             '//div[text()="Price/Book"]/following-sibling::div/div/div'
         )
-        time.sleep(1)
-        stock_price = self.pyppeteer_extensions.get_text(
+        while not pb_ratio:
+            pb_ratio = self.pyppeteer_extensions.get_text(
+                '//div[text()="Price/Book"]/following-sibling::div/div/div'
+            )
+        self.pyppeteer_extensions.wait_for_element(
             f'//span[text()="{ticker}"]/parent::div/div/label'
         )
+        while not stock_price:
+            stock_price = self.pyppeteer_extensions.get_text(
+                f'//span[text()="{ticker}"]/parent::div/div/label'
+            )
         time.sleep(2)
+        self.pyppeteer_extensions.wait_for_element(
+            '//div[text()="Financial Analysis"]/parent::div/parent::div'
+        )
         self.pyppeteer_extensions.click(
             '//div[text()="Financial Analysis"]/parent::div/parent::div'
         )
         time.sleep(1)
         self.pyppeteer_extensions.click('//div[text()="Profitability"]')
         self.pyppeteer_extensions.wait_for_element(
-            '(//div[text()="Current Ratio"]/parent::div/parent::div/parent::div/parent::div/div)[33]'
+            '(//div[text()="Current Ratio"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
         )
-        current_ratio = self.pyppeteer_extensions.get_text(
-            '(//div[text()="Current Ratio"]/parent::div/parent::div/parent::div/parent::div/div)[33]'
+        while not current_ratio:
+            current_ratio = self.pyppeteer_extensions.get_text(
+                '(//div[text()="Current Ratio"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
+            )
+        self.pyppeteer_extensions.wait_for_element(
+            '(//div[text()="Return On Equity"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
         )
-        time.sleep(1)
-        return_on_equity = self.pyppeteer_extensions.get_text(
-            '(//div[text()="Return On Equity"]/parent::div/parent::div/parent::div/parent::div/div)[12]/div/div/div'
+        while not return_on_equity:
+            return_on_equity = self.pyppeteer_extensions.get_text(
+                '(//div[text()="Return On Equity"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
+            )
+        self.pyppeteer_extensions.wait_for_element(
+            '(//div[text()="Asset Turnover"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
         )
-        time.sleep(1)
-        asset_turnover = self.pyppeteer_extensions.get_text(
-            '(//div[text()="Asset Turnover"]/parent::div/parent::div/parent::div/parent::div/div)[12]/div/div/div'
-        )
+        while not asset_turnover:
+            asset_turnover = self.pyppeteer_extensions.get_text(
+                '(//div[text()="Asset Turnover"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
+            )
         time.sleep(1)
         self.pyppeteer_extensions.click('//span[text()="Highlights"]')
         self.pyppeteer_extensions.wait_for_element(
-            '(//div[text()="Diluted EPS"]/parent::div/parent::div/parent::div/parent::div/div)[44]'
+            '(//div[text()="Diluted EPS"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
         )
-        eps_current_fiscal_year = self.pyppeteer_extensions.get_text(
-            '(//div[text()="Diluted EPS"]/parent::div/parent::div/parent::div/parent::div/div)[44]'
+        while not eps_current_fiscal_year:
+            eps_current_fiscal_year = self.pyppeteer_extensions.get_text(
+                '(//div[text()="Diluted EPS"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
+            )
+        self.pyppeteer_extensions.wait_for_element(
+            '(//div[text()="Diluted EPS"]/parent::div/parent::div/parent::div/parent::div/div)[position()=last()-3]'
         )
-        time.sleep(1)
-        eps_last_fiscal_year = self.pyppeteer_extensions.get_text(
-            '(//div[text()="Diluted EPS"]/parent::div/parent::div/parent::div/parent::div/div)[43]'
-        )
+        while not eps_last_fiscal_year:
+            eps_last_fiscal_year = self.pyppeteer_extensions.get_text(
+                '(//div[text()="Diluted EPS"]/parent::div/parent::div/parent::div/parent::div/div)[position()=last()-3]'
+            )
         time.sleep(2)
         self.pyppeteer_extensions.click('//span[text()="Solvency"]')
         self.pyppeteer_extensions.wait_for_element(
-            '(//div[text()="Total Liabilities / Total Assets"]/parent::div/parent::div/parent::div/parent::div/div)[44]'
+            '(//div[text()="Total Liabilities / Total Assets"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
         )
-        total_liabilities_total_assets = self.pyppeteer_extensions.get_text(
-            '(//div[text()="Total Liabilities / Total Assets"]/parent::div/parent::div/parent::div/parent::div/div)[44]'
-        )
+        while not total_liabilities_total_assets:
+            total_liabilities_total_assets = self.pyppeteer_extensions.get_text(
+                '(//div[text()="Total Liabilities / Total Assets"]/parent::div/parent::div/parent::div/parent::div/div)[last()]'
+            )
         time.sleep(3)
-
+        # Clean up the data
+        pe_ratio = pe_ratio.replace(",", "")
+        pb_ratio = pb_ratio.replace(",", "")
+        stock_price = stock_price.replace(",", "")
+        current_ratio = current_ratio.replace(",", "")
+        return_on_equity = return_on_equity.replace(",", "")
+        asset_turnover = asset_turnover.replace(",", "")
+        eps_current_fiscal_year = eps_current_fiscal_year.replace(",", "")
+        eps_last_fiscal_year = eps_last_fiscal_year.replace(",", "")
+        total_liabilities_total_assets = total_liabilities_total_assets.replace(",", "")
+        asset_turnover = asset_turnover.replace("x", "")
+        current_ratio = current_ratio.replace("x", "")
+        return_on_equity = return_on_equity.replace("%", "")
+        total_liabilities_total_assets = total_liabilities_total_assets.replace("%", "")
+        pe_ratio = float(pe_ratio) if pe_ratio != "Upgrade" else pe_ratio
+        pb_ratio = float(pb_ratio) if pb_ratio != "Upgrade" else pb_ratio
+        stock_price = float(stock_price) if stock_price != "Upgrade" else stock_price
+        current_ratio = (
+            float(current_ratio) if current_ratio != "Upgrade" else current_ratio
+        )
+        return_on_equity = (
+            float(return_on_equity)
+            if return_on_equity != "Upgrade"
+            else return_on_equity
+        )
+        asset_turnover = (
+            float(asset_turnover) if asset_turnover != "Upgrade" else asset_turnover
+        )
+        eps_current_fiscal_year = (
+            float(eps_current_fiscal_year)
+            if eps_current_fiscal_year != "Upgrade"
+            else eps_current_fiscal_year
+        )
+        eps_last_fiscal_year = (
+            float(eps_last_fiscal_year)
+            if eps_last_fiscal_year != "Upgrade"
+            else eps_last_fiscal_year
+        )
+        total_liabilities_total_assets = (
+            float(total_liabilities_total_assets)
+            if total_liabilities_total_assets != "Upgrade"
+            else total_liabilities_total_assets
+        )
         # Crear un lock dentro del proceso hijo
         lock = threading.Lock()
         with lock:
+            # Write the data to the Excel file
             self.write_to_excel(
                 char,
                 pe_ratio,
